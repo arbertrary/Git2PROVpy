bc7468e875cc2de07a39aae8081314595c84ce93
arbertrary <armin.bernstetter@gmail.com>
2022-10-26 16:04:13 +0200

    removed some comments, added commit ids to iterate repo output

--- src/git2provConverter.py
+++ src/git2provConverter.py
@@ -1,7 +1,10 @@
 import imp
 from os import path
+from typing import Tuple, Set
 import pygit2
 import prov.model as prov
+
+import re
 
 import shutil
 
@@ -10,28 +13,12 @@ def convert(giturl:str, serialization, repositoryPath, requestUrl, options, call
         giturl = giturl.replace("git@github.com:", "https://github.com/")
 
     repoName = giturl.split("/")[-1].removesuffix(".git")
-    # print(repoName)
-
 
     repo = clone(giturl, repositoryPath)
-    iterate_repository(repo)
+    # iterate_repository(repo)
 
-    # convertRepositoryToProv(repo, serialization, requestUrl, options)
-
-    # def callback1(error):
-    #     if (error):
-    #         callback(None, error)
-    #         # shutil.rmtree(repositoryPath)
-            
-    #     else:
-    #         def callback2(prov, contentType):
-    #             callback(prov, None, contentType)
-    #             # shutil.rmtree(repositoryPath)
-    #         convertRepositoryToProv(repositoryPath, serialization, requestUrl, options, callback2)
-
-    # clone(giturl, repositoryPath, callback1)
-
-    # getProvObject()
+    provObject = convertRepositoryToProv(repo, serialization, requestUrl, options)
+    print(provObject.serialize(indent=2))
 
 
 def getPrefixes(urlprefix:str, requestUrl:str, serialization):
@@ -53,36 +40,39 @@ def convertRepositoryToProv(repo:pygit2.Repository, serialization, requestUrl, o
     prefixes = getPrefixes(urlprefix, requestUrl, serialization)
 
     provObject = getProvObject()
+    bundle = list(provObject.bundles)[0]
+    
 
+    fileList = iterate_repository(repo)
+    for name in set([x[2] for x in fileList]):
+        #  Because all identifiers need to be QNames in PROV, and we need valid turtle as well, we need to get rid of the slashes, spaces and dots
+        currentEntity = re.sub(r'[\/. ]',"-", name) 
 
-    # fileList = 
+        # add entity to prov
+        bundle.entity(urlprefix+":file-"+currentEntity, {"prov:label":name})
 
-    print("convertRepositoryToProv")
+    return provObject
 
-def iterate_tree(tree:pygit2.Tree, fileList:set, prefix:str=""):
+def iterate_tree(tree:pygit2.Tree, fileList:Set[Tuple[pygit2.Oid, str]],commit_id, prefix:str=""):
     for obj in tree:
-        # print(obj.id, obj.type_str, obj.name)
         if (obj.type_str == "blob"):
-            fileList.add(prefix+ obj.name)
+            fileList.add((commit_id, obj.id, prefix + obj.name))
         else:
-            iterate_tree(obj, fileList, prefix=obj.name+"/")
+            iterate_tree(obj, fileList,commit_id, prefix=obj.name+"/")
     return fileList
-            
 
 def iterate_repository(repo):
-    fileSet:set = set()
+    fileSet:Set[Tuple[pygit2.Oid, str]] = set()
     print("# ITERATE REPO")
     for branch_name in list(repo.branches):
         branch = repo.branches.get(branch_name)
 
         latest_commit = branch.target
-        # print(type(latest_commit))
-        # latest_commit_id = repo.revparse_single(str(latest_commit))
-        # print(latest_commit_id)
+
         if (type(latest_commit) != str):
             try:
                 for commit in repo.walk(latest_commit, pygit2.GIT_SORT_TIME):
-                    fileList = iterate_tree(commit.tree, fileSet)
+                    fileList = iterate_tree(commit.tree, fileSet, commit.id)
             except ValueError:
                 print("##### START ERROR")
                 print(branch_name)
@@ -90,6 +80,7 @@ def iterate_repository(repo):
                 print(latest_commit)
                 print("##### END ERROR")
     print(fileSet)
+    return fileSet
 
 def clone(giturl:str, repositoryPath:path):
     print("clone")
@@ -99,10 +90,8 @@ def clone(giturl:str, repositoryPath:path):
         else:
             repo = pygit2.clone_repository(giturl, repositoryPath)
 
-        # callback(None)
     except Exception as e:
         print(e)
-        # callback(e)
     
     return repo
 
@@ -119,7 +108,7 @@ def getProvObject():
     provBundle = provObject.bundle("result:provenance")
     # altBundle = provObject.bundle("fullResult:provenance")
 
-    provBundle.entity("result:file-screenshots")
+    # provBundle.entity("result:file-screenshots")
     # Entity: now:employment-article-v1.html
     # Agent: nowpeople:Bob
     # provObject.agent('result:Bob')
